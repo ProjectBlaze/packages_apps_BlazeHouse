16ee2180c68dd6b892b331c094ce34cf54c7ecb3
Pranav Vashi <neobuddy89@gmail.com>
2022-03-10 02:54:36 +0530

    BlazeHouse: Add reset settings for UDFPS
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- src/com/blaze/house/fragments/UdfpsSettings.java
+++ src/com/blaze/house/fragments/UdfpsSettings.java
@@ -89,6 +89,8 @@ public class UdfpsSettings extends SettingsPreferenceFragment {
                 Settings.System.UDFPS_ANIM_STYLE, 0, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.UDFPS_ICON, 0, UserHandle.USER_CURRENT);
+        Settings.Secure.putIntForUser(resolver,
+                Settings.Secure.SCREEN_OFF_UDFPS_ENABLED, 1, UserHandle.USER_CURRENT);
     }
 
     @Override
