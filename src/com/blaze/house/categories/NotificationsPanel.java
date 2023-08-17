2f7dd08b443831b516e0f715959124b4d3d4cfbf
minaripenguin <minaripenguin@users.noreply.github.com>
2023-08-17 23:12:05 +0530

    BlazeHouse: A11 QS UI Style [3/3]
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Change-Id: Id364d63b79b521fbccbdab62af719d80e83eb80f

--- src/com/blaze/house/categories/NotificationsPanel.java
+++ src/com/blaze/house/categories/NotificationsPanel.java
@@ -17,20 +17,37 @@
 package com.blaze.house.categories;
 
 import android.content.ContentResolver;
+import android.content.Context;
+import android.database.ContentObserver;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.os.Handler;
+import android.provider.Settings;
 
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 
 import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.util.blaze.BlazeUtils;
+import com.android.internal.util.blaze.ThemeUtils;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+
+import com.blaze.house.preferences.CustomSeekBarPreference;
+import com.blaze.house.preferences.SystemSettingListPreference;
 
 public class NotificationsPanel extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
     private static final String TAG = "NotificationsPanel";
+    private static final String KEY_QS_UI_STYLE  = "qs_ui_style";
+
+    private Handler mHandler;
+    private SystemSettingListPreference mQsStyle;
+    private SystemSettingListPreference mQsUI;
+    private ThemeUtils mThemeUtils;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,6 +56,31 @@ public class NotificationsPanel extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.notificationspanel);
 
         ContentResolver resolver = getActivity().getContentResolver();
+
+	mQsUI = (SystemSettingListPreference) findPreference(KEY_QS_UI_STYLE);
+    }
+
+    private CustomSettingsObserver mCustomSettingsObserver = new CustomSettingsObserver(mHandler);
+    private class CustomSettingsObserver extends ContentObserver {
+
+        CustomSettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            Context mContext = getContext();
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QS_UI_STYLE),
+                    false, this, UserHandle.USER_ALL);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            if (uri.equals(Settings.System.getUriFor(Settings.System.QS_UI_STYLE))) {
+                updateQsStyle(true /*QS UI theme*/);
+            }
+        }
     }
 
     @Override
@@ -58,6 +100,32 @@ public class NotificationsPanel extends SettingsPreferenceFragment implements
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
         final String key = preference.getKey();
+	if (preference == mQsUI) {
+            mCustomSettingsObserver.observe();
+            return true;
+        }
         return true;
+    }
+
+	public static void reset(Context mContext) {
+        ContentResolver resolver = mContext.getContentResolver();
+		Settings.System.putIntForUser(resolver,
+                Settings.System.QS_UI_STYLE, 0, UserHandle.USER_CURRENT);
+    }
+
+	private void updateQsStyle(boolean isQsUI) {
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        boolean isA11Style = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.QS_UI_STYLE , 1, UserHandle.USER_CURRENT) == 1;
+	if (isQsUI) {
+	    setQsStyle(isA11Style ? "com.android.system.qs.ui.A11" : "com.android.systemui");
+		}
+	}
+
+	public void setQsStyle(String overlayName) {
+		boolean isA11Style = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.QS_UI_STYLE , 1, UserHandle.USER_CURRENT) == 1;
+        mThemeUtils.setOverlayEnabled("android.theme.customization.qs_ui" , "com.android.systemui");
     }
 }
